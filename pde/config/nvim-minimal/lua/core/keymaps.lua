f208fe50acd80a1eb20fb20a3a8c5011927b7455
hco <henryco4388@gmail.com>
2026-04-24 10:24:27 -0700

    feat(nvim): tab management bindings and diffview in new tab

--- pde/config/nvim-minimal/lua/core/keymaps.lua
+++ pde/config/nvim-minimal/lua/core/keymaps.lua
@@ -6,10 +6,18 @@ map("n", "<C-j>", "<C-w>j")
 map("n", "<C-k>", "<C-w>k")
 map("n", "<C-l>", "<C-w>l")
 
--- bufferline
-map("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>")
-map("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>")
-map("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete buffer" })
+-- buffers
+map("n", "<S-h>",       "<cmd>BufferLineCyclePrev<cr>",  { desc = "Prev buffer" })
+map("n", "<S-l>",       "<cmd>BufferLineCycleNext<cr>",  { desc = "Next buffer" })
+map("n", "<leader>bb",  "<cmd>FzfLua buffers<cr>",       { desc = "Pick buffer" })
+map("n", "<leader>bd",  "<cmd>bdelete<cr>",              { desc = "Delete buffer" })
+map("n", "<leader>bD",  "<cmd>bdelete!<cr>",             { desc = "Delete buffer (force)" })
+map("n", "<leader>bo",  "<cmd>%bd|e#|bd#<cr>",           { desc = "Delete other buffers" })
+map("n", "<leader>bn",  "<cmd>bnext<cr>",                { desc = "Next buffer" })
+map("n", "<leader>bp",  "<cmd>bprevious<cr>",            { desc = "Prev buffer" })
+map("n", "<leader>bw",  "<cmd>write<cr>",                { desc = "Write buffer" })
+map("n", "<leader>bW",  "<cmd>wall<cr>",                 { desc = "Write all buffers" })
+map("n", "<leader>bP",  "<cmd>BufferLineTogglePin<cr>",  { desc = "Pin buffer" })
 
 map("n", "<leader><leader>", "<cmd>FzfLua files<cr>", { desc = "Find files" })
 
@@ -19,6 +27,16 @@ map("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>", { desc = "Live grep" })
 map("n", "<leader>fb", "<cmd>FzfLua buffers<cr>", { desc = "Buffers" })
 map("n", "<leader>fr", "<cmd>FzfLua oldfiles<cr>", { desc = "Recent files" })
 map("n", "<leader>/", "<cmd>FzfLua blines<cr>", { desc = "Search buffer" })
+
+-- tabs
+map("n", "<leader><Tab><Tab>", "<cmd>tabnew<cr>",       { desc = "New tab" })
+map("n", "<leader><Tab>d",     "<cmd>tabclose<cr>",     { desc = "Close tab" })
+map("n", "<leader><Tab>o",     "<cmd>tabonly<cr>",      { desc = "Close other tabs" })
+map("n", "<leader><Tab>l",     "<cmd>tabnext<cr>",      { desc = "Next tab" })
+map("n", "<leader><Tab>h",     "<cmd>tabprevious<cr>",  { desc = "Prev tab" })
+map("n", "<leader><Tab>f",     "<cmd>tabfirst<cr>",     { desc = "First tab" })
+map("n", "<leader><Tab>L",     "<cmd>tablast<cr>",      { desc = "Last tab" })
+map("n", "<leader><Tab>T",     "<C-w>T",                { desc = "Move window to new tab" })
 
 -- terminal
 map("t", "<C-q>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
--- pde/config/nvim-minimal/lua/plugins/diffview.lua
+++ pde/config/nvim-minimal/lua/plugins/diffview.lua
@@ -7,7 +7,7 @@ require("diffview").setup({
 })
 
 local map = vim.keymap.set
-map("n", "<leader>gv", "<cmd>DiffviewOpen<cr>",               { desc = "Diffview open" })
+map("n", "<leader>gv", "<cmd>tabnew | DiffviewOpen<cr>",      { desc = "Diffview open (new tab)" })
 map("n", "<leader>gV", "<cmd>DiffviewClose<cr>",              { desc = "Diffview close" })
 map("n", "<leader>gH", "<cmd>DiffviewFileHistory<cr>",        { desc = "Diffview repo history" })
 map("n", "<leader>gF", "<cmd>DiffviewFileHistory %<cr>",      { desc = "Diffview file history" })
