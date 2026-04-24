1b611740a36688770e1b20691ee3219509b8f307
hco <henryco4388@gmail.com>
2026-04-24 08:37:44 -0700

    feat(nvim): register pi as blink.cmp source in chat prompt

--- pde/config/nvim-minimal/lua/plugins/completion.lua
+++ pde/config/nvim-minimal/lua/plugins/completion.lua
@@ -2,6 +2,12 @@ require("blink.cmp").setup({
   keymap = { preset = "default" },
   sources = {
     default = { "lsp", "path", "buffer" },
+    per_filetype = {
+      ["pi-chat-prompt"] = { "pi" },
+    },
+    providers = {
+      pi = { name = "Pi", module = "pi.completion.blink" },
+    },
   },
   completion = {
     documentation = { auto_show = true },
